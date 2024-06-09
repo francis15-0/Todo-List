19f5ee6368c7ec4440f8d9d9f97ead5f8f0aa5ab
francis15-0 <nnadiekwemakuo.11@gmail.com>
2024-06-09 10:07:24 -0400

    Created a term state that stores the value for the input

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,33 @@
-export default function TodoForm({mode}){
-    return(
-        <>
-            <form action="">
-                <input className={mode? "dark": "light"} type="text" name="" id="" />
-                <div></div>
-            </form>
-        </>
-    )
+import { useState } from "react";
+
+export default function TodoForm({ mode }) {
+    const [term, setTerm] = useState('')
+    const handleChange = (e)=>{
+        setTerm(e.target.value)
+    }
+  return (
+    <>
+      <form action="">
+        <div className="form-design">
+          <input
+            className={mode ? "dark" : "light"}
+            type="text"
+            name=""
+            id=""
+            value={term}
+            onChange={(e)=> handleChange(e)}
+          />
+          <button
+            className="submit"
+            style={
+              mode
+                ? { backgroundColor: "hsl(235, 24%, 19%)" }
+                : { backgroundColor: "white", border: "1px solid black" }
+            }
+            
+          ></button>
+        </div>
+      </form>
+    </>
+  );
 }
